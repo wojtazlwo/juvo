1bacbdf2a74f14a3bda134946f3edcf1835ba252
Wojciech Reczek <reczeklwo@gmail.com>
2020-09-05 11:02:29 +0200

    test

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Konieczne do dzialania bootstrapa -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS lokalnie-->
+	  <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+    <!-- Font z google -->
+    <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&display=swap" rel="stylesheet">
+    <title>Hello, world!</title>
+    <meta
+  </head>
+  <body>
+    <header>
+      <nav>
+        <!-- Tu będzie belka nawigacyjna. Chciałbym, aby belka podążała za użytkownikiem, lub żeby pojawiała się dopiero przy ponownym podjechaniu w górę -->
+      </nav>
+      <img src=logo.png alt="logo" class="logo">
+    </header>
+    <main>
+      <section class="black">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-3"></div>
+              <img src="statue.png" class="img-left">
+              ą
+            <div class="col-sm-3">
+              <p>Kim jesteśmy?</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus ex eget dolor volutpat viverra. Nam accumsan, leo at accumsan pretium, orci massa pellentesque enim, molestie commodo leo quam vel risus. Praesent porta egestas ultricies. Vivamus sed elementum est. Vivamus egestas rhoncus bibendum. Donec augue enim, ultricies vitae vestibulum non, cursus ac tellus. Etiam eu dui eget mauris pharetra lobortis. Pellentesque vitae aliquam ipsum</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="white">
+        <img src="deer.png" class="img-right">
+      </section>
+    </main>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery, Popper.js, Bootstrap JS lokalnie -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
