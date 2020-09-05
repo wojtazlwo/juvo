c1ee33a17db8e0ad57ad65c4f631749d2d9e7dca
Wojciech Reczek <reczeklwo@gmail.com>
2020-09-05 14:38:45 +0200

    dalsze prace nad stroną, pozbywanie się bootstrapa póki nie jest potrzebny

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Konieczne do dzialania bootstrapa -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS lokalnie-->
+    <!-- w razie gdy będzie potrzebny bootstrap
 	  <link rel="stylesheet" href="css/bootstrap.min.css">
+    -->
     <link rel="stylesheet" href="main.css">
     <!-- Font z google -->
     <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&display=swap" rel="stylesheet">
@@ -18,31 +19,34 @@
       <nav>
         <!-- Tu będzie belka nawigacyjna. Chciałbym, aby belka podążała za użytkownikiem, lub żeby pojawiała się dopiero przy ponownym podjechaniu w górę -->
       </nav>
-      <img src=logo.png alt="logo" class="logo">
+      <img src=logo.png alt="logo" id="logo">
     </header>
     <main>
       <section class="black">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm-3"></div>
-              <img src="statue.png" class="img-left">
-              ą
-            <div class="col-sm-3">
-              <p>Kim jesteśmy?</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus ex eget dolor volutpat viverra. Nam accumsan, leo at accumsan pretium, orci massa pellentesque enim, molestie commodo leo quam vel risus. Praesent porta egestas ultricies. Vivamus sed elementum est. Vivamus egestas rhoncus bibendum. Donec augue enim, ultricies vitae vestibulum non, cursus ac tellus. Etiam eu dui eget mauris pharetra lobortis. Pellentesque vitae aliquam ipsum</p>
-            </div>
-          </div>
-        </div>
+        <div class = "heading">
+          <h1>Kim jesteśmy?</h1>
+        <div/>
+        <div class = "message">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam est odio, finibus quis tempor sed, placerat eu nisl. Nunc non orci sed magna laoreet lobortis pulvinar volutpat enim. Donec diam urna, tempor eu sagittis sed, consequat et quam. Nullam urna lacus, faucibus non purus at, ultrices porttitor eros. Pellentesque dapibus sollicitudin elit, sed hendrerit ligula mattis ac. Curabitur semper feugiat erat id malesuada. Nunc bibendum tincidunt erat, sed tempor dolor luctus eu. Ut id egestas justo. Aliquam erat volutpat.
+        <div/>
+        <img src="statue.png">
       </section>
       <section class="white">
-        <img src="deer.png" class="img-right">
+        <div class = "heading">
+          <h1>Kim jesteśmy?</h1>
+        <div/>
+        <div class = "message">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam est odio, finibus quis tempor sed, placerat eu nisl. Nunc non orci sed magna laoreet lobortis pulvinar volutpat enim. Donec diam urna, tempor eu sagittis sed, consequat et quam. Nullam urna lacus, faucibus non purus at, ultrices porttitor eros. Pellentesque dapibus sollicitudin elit, sed hendrerit ligula mattis ac. Curabitur semper feugiat erat id malesuada. Nunc bibendum tincidunt erat, sed tempor dolor luctus eu. Ut id egestas justo. Aliquam erat volutpat.
+        <div/>
+        <img src="deer.png">
       </section>
     </main>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery, Popper.js, Bootstrap JS lokalnie -->
+
+    <!-- jQuery, Popper.js, Bootstrap JS lokalnie, także w razie potrzeby bootstrapa
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="js/bootstrap.min.js"></script>
+  -->
   </body>
 </html>
